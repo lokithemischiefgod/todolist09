9877b549480f6abf73e414302f2ba708fd5207d2
Farhann30 <rezfeckfarhan@gmail.com>
2024-12-17 21:50:32 +0800

    add Migration

---
+++ todolist/migrations/0004.py
@@ -0,0 +1,17 @@
+import django.core.validators
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('todoliat', '0003'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='todolist',
+            name='content',
+            field=models.CharField(max_length=50, validators=[django.core.validators.MinLengthValidator(2)]),
+        ),
+    ]
