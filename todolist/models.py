5ece608059aaaa4ee80e802a3a30fffd1263040c
Loki <captainindonesia3@gmail.com>
2024-12-18 20:03:47 +0800

    fix time if data got edited

--- todolist/models.py
+++ todolist/models.py
@@ -8,7 +8,7 @@ class Todo(models.Model):
 
     content = models.TextField()
     title = models.CharField(max_length=50)
-    date = models.DateTimeField(auto_now_add=True)
+    date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.content
